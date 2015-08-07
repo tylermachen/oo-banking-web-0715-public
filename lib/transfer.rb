aa0063427ec9cc1d0d25d574410aaad71abe8113
Tyler Machen <tylermachen@gmail.com>
2015-08-07 13:37:17 -0400

    complete banking classes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Transfer
-  # code here
+  attr_reader :status, :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender =   sender
+    @receiver = receiver
+    @status =   'pending'
+    @amount =   amount
+  end
+
+  def both_valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    unless @status == 'complete'
+      if sender.balance >= @amount
+        sender.balance -= @amount
+        receiver.balance += @amount
+        @status = 'complete'
+      else
+        @status = 'rejected'
+        'Transaction rejected. Please check your account balance.'
+      end
+    end
+  end
+
+  def reverse_transfer
+    if @status == 'complete'
+      sender.balance += @amount
+      receiver.balance -= @amount
+      @status = 'reversed'
+    end
+  end
 end
